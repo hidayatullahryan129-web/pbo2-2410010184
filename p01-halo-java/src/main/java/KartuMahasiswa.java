abeea122d805649ba48c017a17c2749bc2e76fdc
M. Ryan Hidayatullah <hidayatullahryan129@gmail.com>
2026-09-23 20:06:21 +0800

    P01: menambahkan KartuMahasiswa

---
+++ p01-halo-java/src/main/java/KartuMahasiswa.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class KartuMahasiswa {
+    
+    public static void main(String[] args) {
+        
+        String nama = "M. Ryan Hidayatullah";
+        String npm = "2410010184";
+        String prodi = "Teknik Informatika";
+        int semester = 5;
+        String alasan = "Ingin memahami pemrograman berbasis objek";
+
+        System.out.println("==============================");
+        System.out.println("     KARTU MAHASISWA PBO 2");
+        System.out.println("==============================");
+        System.out.println("Nama     : " + nama);
+        System.out.println("NPM      : " + npm);
+        System.out.println("Prodi    : " + prodi);
+        System.out.println("Semester : " + semester);
+        System.out.println("Alasan   : " + alasan);
+        System.out.println("==============================");
+    }
+    
+}
